b1e834d7bf9521a65d275a970ba61a19520aaefb
Tanmay Jain <tanmayjain@Tanmays-MacBook-Air.local>
2018-01-18 17:42:37 +0530

    Routes written

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'libraries/index'
+
+  get 'library/index'
+  resources :libraries
+  resources :categories
+  resources :members
+  resources :issue_histories
+  resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
